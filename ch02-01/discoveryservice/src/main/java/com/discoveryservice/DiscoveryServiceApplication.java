9065644526993102ed238085cee2358a2191e737
kiosk123 <heo9910@naver.com>
2022-06-01 05:50:02 +0900

    .

--- ch02-01/discoveryservice/src/main/java/com/discoveryservice/DiscoveryServiceApplication.java
+++ ch02-01/discoveryservice/src/main/java/com/discoveryservice/DiscoveryServiceApplication.java
@@ -2,14 +2,12 @@ package com.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryServiceApplication {
+public class DiscoveryserviceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserviceApplication.class, args);
+	}
 
 }
